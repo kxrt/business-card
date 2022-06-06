3839aa7a019e074a0a7faaf2dd83864ef18d001f
kxrt <kvrtikeya@gmail.com>
2022-06-06 13:46:10 +0800

    Added card information

---
+++ README.md
@@ -0,0 +1,12 @@
+It's me, Kartikeya!
+
+# Usage
+
+```bash
+npx kxrt 
+```
+
+# Credit
+
+Forked from [@bitandbang](https://twitter.com/bitandbang/status/1075473070368919552)
+([repo](https://github.com/bnb/bitandbang)) via [@natterstefan](https://github.com/natterstefan/)
---
+++ bin/card.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round',
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('Kartikeya'),
+  handle: chalk.cyan('@kxrt'),
+  labelWork: chalk.white.bold('Profession:'),
+  work: chalk.white('Student, National University of Singapore'),
+  labelGitHub: chalk.white.bold('    GitHub:'),
+  github: chalk.cyan('https://github.com/kxrt'),
+  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  linkedin: chalk.cyan('https://linkedin.com/in/kvrtikeya'),
+  labelWeb: chalk.white.bold('       Web:'),
+  web: chalk.cyan('https://kxrt.github.io'),
+  labelCard: chalk.white.bold('      Card:'),
+  npx: chalk.white('npx kxrt'),
+}
+
+// Actual strings we're going to output
+const newline = '\n'
+const heading = `${data.name} / ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const webing = `${data.labelWeb}  ${data.web}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen
+// effectively
+const output =
+  heading +
+  newline +
+  newline +
+  working +
+  newline +
+  githubing +
+  newline +
+  linkedining +
+  newline +
+  webing +
+  newline +
+  newline +
+  carding
+
+console.log(chalk.green(boxen(output, options)))
